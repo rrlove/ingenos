1f1abdebcf1685feeb3284d3eb91fc6a5b8047bc
rrlove <rlove1@nd.edu>
2017-08-15 13:48:47 -0400

    rearranging directory structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='ingenos',
+     version='0.1',
+     description='classes and methods for inversion data via scikit-allel',
+     url='https://github.com/rrlove/ingenos',
+     author='Becca Love',
+     author_email='rachelrebeccalove@gmail.com',
+     license='GNU General Public License v3.0',
+     packages=['ingenos'],
+     zip_safe=False
+     )
