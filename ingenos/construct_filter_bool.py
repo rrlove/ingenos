ac45857080ce706500f9711aca7cf511d2d0e331
rrlove <rlove1@nd.edu>
2017-08-15 14:15:45 -0400

    reorganizing functions

--- ingenos/construct_filter_bool.py
+++
@@ -1,5 +0,0 @@
-def construct_filter_bool(variants,expression):
-    '''Take a set of variants and a positional expression on which to filter them, return a Boolean as to whether each variant/row of genotypes should be included post-filtering.'''
-
-    selection_bool = variants.eval(expression)[:]
-    return selection_bool
--- ingenos/downsample_genotypes.py
+++
@@ -1,7 +0,0 @@
-def downsample_genotypes(boolean_filter,downsample_rate=10):
-    '''Downsample a set of genotypes for computational tractability.
-    To be precise, this function stochastically shrinks the Boolean filter later used to pull out the genotypes.
-    Shrinkage is approximate.'''
-    
-    downsampled_boolean = [random.randrange(100) < downsample_rate if value else value for value in boolean_filter]
-    return downsampled_boolean
